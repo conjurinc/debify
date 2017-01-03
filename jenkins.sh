6bbc5f4109283a327100b8525ef9e0edeca30aa1
Dustin Collins <dustinrcollins@gmail.com>
2017-01-03 15:52:11 -0600

    Only build the Docker image for master [CON-3751]

--- jenkins.sh
+++ jenkins.sh
@@ -5,10 +5,10 @@ bundle
 rm -rf features/reports
 cucumber --format pretty --format junit --out features/reports || true
 
-docker build -t debify .
-
 if [ "$GIT_BRANCH" == "origin/master" ]; then
   TAG=$(cat lib/conjur/debify/version.rb | grep -o '".*"' | tr -d '"')
+
+  docker build -t debify .
   docker tag debify registry.tld/debify:$TAG
   docker push registry.tld/debify:$TAG
 fi
