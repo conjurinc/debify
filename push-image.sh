96241d59be18e63d7c952a4376d7a93dd7651e38
Alan Potter <alan.potter@conjur.net>
2017-10-03 08:06:22 -0400

    use new debify tag syntax

--- push-image.sh
+++ push-image.sh
@@ -2,7 +2,7 @@
 
 TAG=$(< VERSION)
 
-docker tag -f debify registry.tld/debify:$TAG
-docker tag -f debify registry.tld/debify:latest
+docker tag debify registry.tld/debify:$TAG
+docker tag debify registry.tld/debify:latest
 docker push registry.tld/debify:$TAG
 docker push registry.tld/debify:latest
