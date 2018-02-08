904d95a4e99c30e5af0e4da3a0f0673de7f07759
Alan Potter <alan.potter@conjur.net>
2018-02-08 07:56:16 -0500

    make sure the build will run when it needs to pull an image from the registry

--- ci/test.sh
+++ ci/test.sh
@@ -2,6 +2,11 @@
 
 bundle
 
+# Some tests need to be logged in to the registry, to pull a base
+# image if it's not already available. Have entrypoint.sh do something
+# simple, and log in as a side effect.
+/debify/distrib/entrypoint.sh detect-version
+
 for target in spec cucumber; do
   bundle exec rake $target
 done
