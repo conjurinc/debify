c388a2b4367cb62d782f8e9128d3e3859f89d1ee
Rafał Rzepecki <divided.mind@gmail.com>
2018-05-22 19:43:41 +0200

    Fix a bug with handling spaces in filenames
    When removing files in preparation of the dev package filenames with
    weren't properly handled, leading to duplicate files and dev packages
    failing to install correctly. Fix this by using zero-separated strings
    through the pipeline, and also add a test for this case.

--- lib/conjur/fpm/debify_utils.sh
+++ lib/conjur/fpm/debify_utils.sh
@@ -9,3 +9,24 @@ function bundle_clean() {
     rm -rf vendor/bundle/ruby/${ruby_version}/gems/*/{test,spec,examples,example,contrib,doc,ext,sample}
   fi
 }
+
+# Remove files from the current directory that also exist in another given
+# directory. For example, say in the current directory there is:
+#    foo
+#    bar/baz
+#    bar/xyzzy
+#    bacon
+#    people/phlebas
+# and in dir2 there is
+#    bacon
+#    alice
+#    people/phlebas
+#    bar/xyzzy
+# then after running `remove_matching dir2` current directory will be left with only:
+#    foo
+#    bar/baz
+# Note it probably isn't 100% fool-proof, so don't launch it out to space or something.
+function remove_matching() {
+  find "$1" -type f -print0 | sed -ze "s@^$1@.@" | xargs -0 rm -f
+  find . -type d -empty -delete
+}
---
+++ spec/debify_utils_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+require 'aruba/rspec'
+
+Aruba.configure do |c|
+  c.activate_announcer_on_command_failure = %i(stderr stdout)
+end
+
+describe "remove_matching()", type: :aruba do
+  it "removes matching files" do
+    here %w(foo bar/baz bar/xyzzy zork)
+    there %w(foo bar/baz not)
+    remove_matching
+    expect(contents_of herepath).to match_array %w(zork bar bar/xyzzy)
+  end
+  
+  it "also handles files with spaces in names" do
+    here ['foo', 'bar/baz', 'with space', 'with', 'bar/another space']
+    there ['with space', 'bar/another space here']
+    remove_matching
+    expect(contents_of herepath).to match_array ['foo', 'bar', 'bar/baz', 'with', 'bar/another space']
+  end
+  
+  # auxiliary methods and setup
+  let(:herepath) { Pathname.new Dir.mktmpdir }
+  let(:therepath) { Pathname.new Dir.mktmpdir }
+  after { [herepath, therepath].each &FileUtils.method(:remove_entry) }
+
+  def contents_of dir
+    Dir.chdir(dir) { Dir['**/*'] }
+  end
+
+  def remove_matching
+    run_simple "bash -c 'source #{DEBIFY_UTILS_PATH}; cd #{herepath}; remove_matching #{therepath}'"
+  end
+  
+  def here files
+    mkfiles herepath, files
+  end
+  
+  def there files
+    mkfiles therepath, files
+  end
+
+  def mkfiles dir, files  
+    return dir if files.empty?
+    files.each do |path|
+      fullpath = dir + path
+      FileUtils.makedirs fullpath.dirname
+      FileUtils.touch fullpath
+    end
+  end
+  
+  DEBIFY_UTILS_PATH = File.expand_path '../../lib/conjur/fpm/debify_utils.sh', __FILE__
+end
+
