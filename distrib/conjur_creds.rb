1914617915f7925037513e3873a71cbdd9e9d704
Alan Potter <alan.potter@conjur.net>
2018-06-21 07:34:11 -0400

    get rid of warnings when CONJURRC isn't set, and /etc/conjur.conf doesn't exist

--- distrib/conjur_creds.rb
+++ distrib/conjur_creds.rb
@@ -3,5 +3,7 @@ Netrc.configure do |c|
   c[:allow_permissive_netrc_file] = true
 end
 
-creds = Netrc.read('/root/.netrc')[ENV['CONJUR_APPLIANCE_URL'] + '/authn']
-print "#{creds.login} #{creds.password}" if creds
+if File.exist?('/root/.netrc') && ENV['CONJUR_APPLIANCE_URL']
+  creds = Netrc.read('/root/.netrc')[ENV['CONJUR_APPLIANCE_URL'] + '/authn'] 
+  print "#{creds.login} #{creds.password}" if creds
+end
