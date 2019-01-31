ab683aa74a2e397d8f35196d5730b973f1480d76
Alan Potter <alan.potter@conjur.net>
2019-01-31 07:01:18 -0500

    tag branch image so it can be used for dev
    When debify is built on a branch, tag the
    resulting image so it can be used by other
    projects.

--- push-image.sh
+++ push-image.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -ex
 
-IFS=. read MAJOR MINOR PATCH <VERSION
-
-TAGS="latest $(docker images --filter reference="registry.tld/conjurinc/debify:$MAJOR.$MINOR*" --format '{{.Tag}}')"
-for t in $TAGS; do
+for t in $(./image-tags); do
   docker push registry.tld/conjurinc/debify:$t
 done
 
