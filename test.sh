0251923178e0d64c3ee2ba7c60946598dcf25644
Alon Carmel <alon.carmel@cyberark.com>
2020-07-23 11:16:53 +0300

    update conjur base image

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-
+docker pull registry.tld/cyberark/phusion-ruby-fips:0.11-d243f6c
 VERSION=$(< VERSION)
 docker run --rm debify:$VERSION config script > docker-debify
 chmod +x docker-debify
