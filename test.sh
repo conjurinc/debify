be313a1a0e473e98594eda5e7c1d5e1bb1d8925d
Micah Lee <micah.lee@cyberark.com>
2020-10-23 10:39:22 -0400

    Use branch build of base image

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+docker pull registry.tld/cyberark/phusion-ruby-fips:0.11-6228320
+
 VERSION=$(< VERSION)
 docker run --rm debify:$VERSION config script > docker-debify
 chmod +x docker-debify
