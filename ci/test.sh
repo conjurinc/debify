3d2b67b8413c88363c27cc0ede78c08bc9d71661
Micah Lee <micah.lee@cyberark.com>
2020-12-15 14:39:59 -0500

    dev! trace docker command output

--- ci/test.sh
+++ ci/test.sh
@@ -7,7 +7,14 @@ bundle
 # simple, and log in as a side effect.
 /debify/distrib/entrypoint.sh detect-version
 
-for target in spec cucumber; do
-  bundle exec rake $target
-done
+# for target in spec cucumber; do
+#   bundle exec rake $target
+# done
 
+# bundle exec rake cucumber
+
+apt-get install -y strace
+
+strace -o ./trace.out docker pull registry.tld/conjur-appliance:5.0-stable
+
+grep docker < ./trace.out 
