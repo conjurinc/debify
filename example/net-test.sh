1e7a111043131287e4b929e834d558cda8c7c3a0
Micah Lee <micah.lee@cyberark.com>
2020-12-15 13:01:43 -0500

    fixup!

--- example/net-test.sh
+++ example/net-test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -ex
 
-cid=$1
+cid="$1"
 
-docker exec $cid ping -c1 other_host
+docker exec -t "$cid" bash -c "
+  apt-get update && \
+  DEBIAN_FRONTEND=noninteractive apt-get install -y iputils-ping
+"
+docker exec "$cid" ping -c1 other_host
 
 echo Test succeeded
